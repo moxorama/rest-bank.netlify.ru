408495a6c35d1fcaaea2572af1c2a10f1b2e65e5
George Wizeman <Wizeman@WizeMac.local>
2020-07-17 19:17:39 +0300

    Обновлен swagger, добавлен UUID в ответ

--- spec/integration/transfer_spec.rb
+++ spec/integration/transfer_spec.rb
@@ -12,16 +12,16 @@ describe 'Transfers API' do
   - ok - все ok
   - source - не найден аккаунт отправителя
   - destination - не найден аккаунт получателя
-  - amount - недопустимое значение суммы перевода
+  - negative_amount - недопустимое значение суммы перевода
   - no_balance - не хватает средств на аккаунте отправителя
-  - concurrency - перевод не прошел - попытка нескольких одновременных переводов с этого аккаунта, следует повторить перевод позже
+  - 500 - ошибка на сервере
 
 Если транзакция успешна - возвращаются актуальные значения баланса на аккаунтах
 Если транзакция не успешна - возвращаются значения, актуальные на момент запроса и amount = 0
 Если перевод не был сделан, то amount = 0
 
 Если аккаунт не найден, то в ответе будет пустая строка в качестве его номера и 0 баланс
-"
+" 
       parameter name: :source_account_number, in: :query, type: :string, description: 'Номер счета отправителя'
       parameter name: :destination_account_number, in: :query, type: :string, description: 'Номер счета получателя'
       parameter name: :amount, in: :query, type: :integer, description: 'Сумма перевода'
@@ -32,11 +32,12 @@ describe 'Transfers API' do
             status: { type: :string },
             transfer: { type: :object,
               properties: {
+                uuid: { type: :string },
                 source_account_number: { type: :string },
                 source_balance: { type: :integer },
                 destination_account_number: { type: :string },
                 destination_balance: { type: :integer },
-                amount: { type: :integer },
+                amount: { type: :integer }
               }
             }
           }
